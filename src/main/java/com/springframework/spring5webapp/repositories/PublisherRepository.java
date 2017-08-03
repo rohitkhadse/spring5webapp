b3f0b8af7344ddfa9defaaae01d53478f6933e85
rohitkhadse <rohit.a.khadse@gmail.com>
2017-08-03 14:34:30 -0500

    Publisher Entity added

---
+++ src/main/java/com/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,9 @@
+package com.springframework.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.springframework.spring5webapp.model.Publisher;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long>{
+
+}
